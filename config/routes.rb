9b6b7d71714a2bd84cab5377ad5eaf30085f910f
sophusma <masophus@gmail.com>
2023-03-23 16:26:29 +0300

    ttt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :users, only: %i[index show]
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope :api, defaults: { format: :json } do
+    scope :v1 do
+      devise_for :users, defaults: { format: :json }, path: '', path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'signup'
+      },
+      controllers: {
+        sessions: 'api/v1/users/sessions',
+        registrations: 'api/v1/users/registrations'
+      }
+    end
+  end
 end
